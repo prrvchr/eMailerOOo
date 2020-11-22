292426f13b4ecadc80b4da94d7578cd2a17c41cf
User <prrvchr@gmail.com>
2020-11-22 16:14:03 +0100

    git subrepo pull OAuth2OOo
    subrepo:
      subdir:   "OAuth2OOo"
      merged:   "ad7ee72"
    upstream:
      origin:   "https://github.com/prrvchr/OAuth2OOo.git"
      branch:   "master"
      commit:   "ad7ee72"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

--- OAuth2OOo/python/logger.py
+++ OAuth2OOo/python/logger.py
@@ -17,21 +17,23 @@ from unolib import getStringResource
 from .configuration import g_identifier
 
 g_stringResource = {}
-g_pathResource = 'resource'
-g_logger = '%s.Logger' % g_identifier
-g_loggerService = None
+g_logger = None
 g_debugMode = False
+g_logSettings = None
 
 
-def setDebugMode(mode):
-    global g_debugMode
-    g_debugMode = mode
+def setDebugMode(ctx, mode):
+    if mode:
+        _setDebugModeOn(ctx)
+    else:
+        _setDebugModeOff(ctx)
+    _setDebugMode(mode)
 
 def isDebugMode():
     return g_debugMode
 
 def getMessage(ctx, fileresource, resource, format=()):
-    msg = _getResource(ctx, fileresource).resolveString('%s' % resource)
+    msg = _getResource(ctx, fileresource).resolveString(resource)
     if format:
         msg = msg % format
     return msg
@@ -45,88 +47,72 @@ def logMessage(ctx, level, msg, cls=None, method=None):
             logger.logp(level, cls, method, msg)
 
 def clearLogger():
-    global g_loggerService
-    g_loggerService = None
+    global g_logger
+    g_logger = None
 
 def isLoggerEnabled(ctx):
-    level = _getLoggerConfiguration(ctx).LogLevel
-    enabled = _isLoggerEnabled(level)
+    level = _getLogConfig(ctx).LogLevel
+    enabled = _isLogEnabled(level)
     return enabled
 
 def getLoggerSetting(ctx):
-    configuration = _getLoggerConfiguration(ctx)
-    enabled, index = _getLogIndex(configuration)
-    handler = _getLogHandler(configuration)
-    return enabled, index, handler
+    enabled, index, handler = _getLoggerSetting(ctx)
+    return enabled, index, _getState(handler)
 
-def setLoggerSetting(ctx, enabled, index, handler):
-    configuration = _getLoggerConfiguration(ctx)
-    _setLogIndex(configuration, enabled, index)
-    _setLogHandler(configuration, handler, index)
-    if configuration.hasPendingChanges():
-        configuration.commitChanges()
-        clearLogger()
+def setLoggerSetting(ctx, enabled, index, state):
+    handler = _getHandler(state)
+    _setLoggerSetting(ctx, enabled, index, handler)
 
 def getLoggerUrl(ctx):
     url = '$(userurl)/$(loggername).log'
-    settings = _getLoggerConfiguration(ctx).getByName('HandlerSettings')
+    settings = _getLogConfig(ctx).getByName('HandlerSettings')
     if settings.hasByName('FileURL'):
         url = settings.getByName('FileURL')
     service = ctx.ServiceManager.createInstance('com.sun.star.util.PathSubstitution')
-    return service.substituteVariables(url.replace('$(loggername)', g_logger), True)
+    logger = _getLogName()
+    return service.substituteVariables(url.replace('$(loggername)', logger), True)
 
+# Private getter method
 def _getLogger(ctx):
-    if g_loggerService is None:
+    if g_logger is None:
         _setLogger(ctx)
-    return g_loggerService
-
-def _setLogger(ctx):
-    global g_loggerService
-    singleton = '/singletons/com.sun.star.logging.LoggerPool'
-    g_loggerService = ctx.getValueByName(singleton).getNamedLogger(g_logger)
+    return g_logger
 
 def _getResource(ctx, fileresource):
     if fileresource not in g_stringResource:
-        resource = getStringResource(ctx, g_identifier, g_pathResource, fileresource)
+        resource = getStringResource(ctx, g_identifier, _getPathResource(), fileresource)
         g_stringResource[fileresource] = resource
     return g_stringResource[fileresource]
 
-def _getLoggerConfiguration(ctx):
+def _getLogName():
+    return '%s.Logger' % g_identifier
+
+def _getPathResource():
+    return 'resource'
+
+def _getLoggerSetting(ctx):
+    configuration = _getLogConfig(ctx)
+    enabled, index = _getLogIndex(configuration)
+    handler = configuration.DefaultHandler
+    return enabled, index, handler
+
+def _getLogConfig(ctx):
+    logger = _getLogName()
     nodepath = '/org.openoffice.Office.Logging/Settings'
     configuration = getConfiguration(ctx, nodepath, True)
-    if not configuration.hasByName(g_logger):
-        configuration.insertByName(g_logger, configuration.createInstance())
+    if not configuration.hasByName(logger):
+        configuration.insertByName(logger, configuration.createInstance())
         configuration.commitChanges()
-    nodepath += '/%s' % g_logger
+    nodepath += '/%s' % logger
     return getConfiguration(ctx, nodepath, True)
 
 def _getLogIndex(configuration):
     index = 7
     level = configuration.LogLevel
-    enabled = _isLoggerEnabled(level)
+    enabled = _isLogEnabled(level)
     if enabled:
         index = _getLogLevels().index(level)
     return enabled, index
-
-def _setLogIndex(configuration, enabled, index):
-    level = _getLogLevels()[index] if enabled else OFF
-    if configuration.LogLevel != level:
-        configuration.LogLevel = level
-
-def _getLogHandler(configuration):
-    handler = 1 if configuration.DefaultHandler != 'com.sun.star.logging.FileHandler' else 2
-    return handler
-
-def _setLogHandler(configuration, console, index):
-    handler = 'com.sun.star.logging.ConsoleHandler' if console else 'com.sun.star.logging.FileHandler'
-    if configuration.DefaultHandler != handler:
-        configuration.DefaultHandler = handler
-    settings = configuration.getByName('HandlerSettings')
-    if settings.hasByName('Threshold'):
-        if settings.getByName('Threshold') != index:
-            settings.replaceByName('Threshold', index)
-    else:
-        settings.insertByName('Threshold', index)
 
 def _getLogLevels():
     levels = (SEVERE,
@@ -139,5 +125,71 @@ def _getLogLevels():
               ALL)
     return levels
 
-def _isLoggerEnabled(level):
+def _isLogEnabled(level):
     return level != OFF
+
+def _getLogSetting():
+    global g_logSettings
+    enabled = g_logSettings['enabled']
+    index = g_logSettings['index']
+    handler = g_logSettings['handler']
+    g_logSettings = None
+    return enabled, index, handler
+
+def _getDebugSetting():
+    return True, 7, 'com.sun.star.logging.FileHandler'
+
+def _getHandler(state):
+    handlers = {True: 'ConsoleHandler', False: 'FileHandler'}
+    return 'com.sun.star.logging.%s' % handlers.get(state)
+
+def _getState(handler):
+    states = {'com.sun.star.logging.ConsoleHandler' : 1,
+              'com.sun.star.logging.FileHandler': 2}
+    return states.get(handler)
+
+# Private setter method
+def _setLogger(ctx):
+    global g_logger
+    logger = _getLogName()
+    singleton = '/singletons/com.sun.star.logging.LoggerPool'
+    g_logger = ctx.getValueByName(singleton).getNamedLogger(logger)
+
+def _setLoggerSetting(ctx, enabled, index, handler):
+    configuration = _getLogConfig(ctx)
+    _setLogIndex(configuration, enabled, index)
+    _setLogHandler(configuration, handler, index)
+    if configuration.hasPendingChanges():
+        configuration.commitChanges()
+        clearLogger()
+
+def _setLogIndex(configuration, enabled, index):
+    level = _getLogLevels()[index] if enabled else OFF
+    if configuration.LogLevel != level:
+        configuration.LogLevel = level
+
+def _setLogHandler(configuration, handler, index):
+    if configuration.DefaultHandler != handler:
+        configuration.DefaultHandler = handler
+    settings = configuration.getByName('HandlerSettings')
+    if settings.hasByName('Threshold'):
+        if settings.getByName('Threshold') != index:
+            settings.replaceByName('Threshold', index)
+    else:
+        settings.insertByName('Threshold', index)
+
+def _setDebugMode(mode):
+    global g_debugMode
+    g_debugMode = mode
+
+def _setDebugModeOn(ctx):
+    _setLogSetting(*_getLoggerSetting(ctx))
+    _setLoggerSetting(ctx, *_getDebugSetting())
+
+def _setDebugModeOff(ctx):
+    if g_logSettings is not None:
+        _setLoggerSetting(ctx, *_getLogSetting())
+
+def _setLogSetting(enabled, index, handler):
+    global g_logSettings
+    g_logSettings = {'enabled': enabled, 'index': index, 'handler': handler}

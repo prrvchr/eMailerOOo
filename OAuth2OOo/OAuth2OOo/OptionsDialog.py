5473573434c928ff221f44777e93fddff8f1eabe
User <prrvchr@gmail.com>
2020-11-30 13:20:11 +0100

    git subrepo pull OAuth2OOo
    subrepo:
      subdir:   "OAuth2OOo"
      merged:   "56d9a3d"
    upstream:
      origin:   "https://github.com/prrvchr/OAuth2OOo.git"
      branch:   "master"
      commit:   "56d9a3d"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

--- OAuth2OOo/OAuth2OOo/OptionsDialog.py
+++ OAuth2OOo/OAuth2OOo/OptionsDialog.py
@@ -1,6 +1,32 @@
 #!
 # -*- coding: utf_8 -*-
 
+"""
+╔════════════════════════════════════════════════════════════════════════════════════╗
+║                                                                                    ║
+║   Copyright (c) 2020 https://prrvchr.github.io                                     ║
+║                                                                                    ║
+║   Permission is hereby granted, free of charge, to any person obtaining            ║
+║   a copy of this software and associated documentation files (the "Software"),     ║
+║   to deal in the Software without restriction, including without limitation        ║
+║   the rights to use, copy, modify, merge, publish, distribute, sublicense,         ║
+║   and/or sell copies of the Software, and to permit persons to whom the Software   ║
+║   is furnished to do so, subject to the following conditions:                      ║
+║                                                                                    ║
+║   The above copyright notice and this permission notice shall be included in       ║
+║   all copies or substantial portions of the Software.                              ║
+║                                                                                    ║
+║   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,                  ║
+║   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES                  ║
+║   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.        ║
+║   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY             ║
+║   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,             ║
+║   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE       ║
+║   OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                    ║
+║                                                                                    ║
+╚════════════════════════════════════════════════════════════════════════════════════╝
+"""
+
 import uno
 import unohelper
 
@@ -19,6 +45,7 @@ from unolib import getInteractionHandler
 from unolib import InteractionRequest
 from unolib import getOAuth2UserName
 from unolib import getDialog
+from unolib import getExceptionMessage
 
 from oauth2 import getLoggerUrl
 from oauth2 import getLoggerSetting
@@ -196,7 +223,14 @@ class OptionsDialog(unohelper.Base,
         msg = getMessage(self.ctx, g_message, 113, requests.urllib3.__version__)
         logMessage(self.ctx, INFO, msg, "OptionsDialog", "_logInfo()")
         if requests.ssl is None:
-            msg = getMessage(self.ctx, g_message, 115)
+            msg = ''
+            try:
+                import ssl
+            except ImportError as e:
+                print("OptionsDialog._logInfo() 1")
+                msg = getExceptionMessage(e)
+                print("OptionsDialog._logInfo() 2")
+            msg = getMessage(self.ctx, g_message, 115, msg)
         else:
             msg = getMessage(self.ctx, g_message, 114, requests.ssl.OPENSSL_VERSION)
         logMessage(self.ctx, INFO, msg, "OptionsDialog", "_logInfo()")

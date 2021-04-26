d9cc76f1c60cc3caad44848a0cf2aad8ff25b989
User <prrvchr@gmail.com>
2021-04-26 13:53:12 +0200

    git subrepo pull uno
    subrepo:
      subdir:   "uno"
      merged:   "4fedd81"
    upstream:
      origin:   "https://github.com/prrvchr/uno.git"
      branch:   "main"
      commit:   "4fedd81"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

--- uno/lib/uno/dbtool/__init__.py
+++ uno/lib/uno/dbtool/__init__.py
@@ -41,6 +41,7 @@ from .dbtool import getDataSourceCall
 from .dbtool import getDataSourceClassPath
 from .dbtool import getDataSourceConnection
 from .dbtool import getDictFromResult
+from .dbtool import getDriverPropertyInfos
 from .dbtool import getKeyMapFromResult
 from .dbtool import getKeyMapKeyMapFromResult
 from .dbtool import getKeyMapSequenceFromResult

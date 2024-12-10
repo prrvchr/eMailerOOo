085ec83f81511be4af31daa2965501222c78431e
prrvchr <prrvchr@gmail.com>
2024-12-10 04:09:57 +0100

    git subrepo pull uno
    subrepo:
      subdir:   "uno"
      merged:   "a5892166"
    upstream:
      origin:   "https://github.com/prrvchr/uno.git"
      branch:   "main"
      commit:   "a5892166"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"

--- uno/lib/uno/oauth2/oauth2tool.py
+++ uno/lib/uno/oauth2/oauth2tool.py
@@ -62,7 +62,6 @@ def getRequest(ctx, url=None, name=None):
     return request
 
 def setResquestParameter(arguments, request, parameter):
-    setParametersArguments(request.getByName('Parameters'), arguments)
     method = request.getByName('Method')
     if method:
         parameter.Method = method
@@ -82,44 +81,41 @@ def setParametersArguments(parameters, arguments):
         key = parameter.getByName('Name')
         template = parameter.getByName('Template')
         command = parameter.getByName('Command')
-        setParameterArgument(parameter, key, template, command, arguments)
-
-def setParameterArgument(parameter, key, template, command, arguments):
-    if template:
-        _setArgumentTemplate(key, arguments, template)
-    if command:
-        method = command[0]
-        value = arguments[key]
-        if method == 'encodeURI':
-            safe = _getArgumentCommand(command, "~@#$&()*!+=:;,?/'")
-            arguments[key] = parse.quote(value, safe=safe)
-        elif method == 'encodeURIComponent':
-            safe = _getArgumentCommand(command, "~()*!'")
-            arguments[key] = parse.quote(value, safe=safe)
-        elif method == 'base64URL':
-            arguments[key] = base64.urlsafe_b64encode(value)
-        elif method == 'base64':
-            arguments[key] = base64.b64encode(value)
-        elif method == 'decode':
-            encoding = _getArgumentCommand(command, 'utf-8')
-            errors = _getArgumentCommand(command, 'strict', 2)
-            arguments[key] = value.decode(encoding=encoding, errors=errors)
-        elif method == 'encode':
-            encoding = _getArgumentCommand(command, 'utf-8')
-            errors = _getArgumentCommand(command, 'strict', 2)
-            arguments[key] = value.encode(encoding=encoding, errors=errors)
-        elif method == 'replace':
-            arg1 = _getArgumentCommand(command, '')
-            arg2 = _getArgumentCommand(command, '', 2)
-            arg3 = int(_getArgumentCommand(command, -1, 3))
-            arguments[key] = value.replace(arg1, arg2, arg3)
-        elif method == 'strip':
-            arguments[key] = value.strip(_getArgumentCommand(command))
-        elif method == 'rstrip':
-            arguments[key] = value.rstrip(_getArgumentCommand(command))
-        elif method == 'lstrip':
-            arguments[key] = value.lstrip(_getArgumentCommand(command))
-    setParametersArguments(parameter.getByName('Parameters'), arguments)
+        if template:
+            _setArgumentTemplate(key, arguments, template)
+        if key in arguments and command:
+            method = command[0]
+            value = arguments[key]
+            if method == 'encodeURI':
+                safe = _getArgumentCommand(command, "~@#$&()*!+=:;,?/'")
+                arguments[key] = parse.quote(value, safe=safe)
+            elif method == 'encodeURIComponent':
+                safe = _getArgumentCommand(command, "~()*!'")
+                arguments[key] = parse.quote(value, safe=safe)
+            elif method == 'base64URL':
+                arguments[key] = base64.urlsafe_b64encode(value)
+            elif method == 'base64':
+                arguments[key] = base64.b64encode(value)
+            elif method == 'decode':
+                encoding = _getArgumentCommand(command, 'utf-8')
+                errors = _getArgumentCommand(command, 'strict', 2)
+                arguments[key] = value.decode(encoding=encoding, errors=errors)
+            elif method == 'encode':
+                encoding = _getArgumentCommand(command, 'utf-8')
+                errors = _getArgumentCommand(command, 'strict', 2)
+                arguments[key] = value.encode(encoding=encoding, errors=errors)
+            elif method == 'replace':
+                arg1 = _getArgumentCommand(command, '')
+                arg2 = _getArgumentCommand(command, '', 2)
+                arg3 = int(_getArgumentCommand(command, -1, 3))
+                arguments[key] = value.replace(arg1, arg2, arg3)
+            elif method == 'strip':
+                arguments[key] = value.strip(_getArgumentCommand(command))
+            elif method == 'rstrip':
+                arguments[key] = value.rstrip(_getArgumentCommand(command))
+            elif method == 'lstrip':
+                arguments[key] = value.lstrip(_getArgumentCommand(command))
+        setParametersArguments(parameter.getByName('Parameters'), arguments)
 
 def getParserItems(request):
     keys = {}
